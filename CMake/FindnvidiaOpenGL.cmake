d8249e7674cfd4bfe1da445d65205830011ec9d5
Juan José <juanjosegarciacan@gmail.com>
2015-04-21 13:17:40 +0200

    Find Nvidia libraries modified

--- CMake/FindnvidiaOpenGL.cmake
+++ CMake/FindnvidiaOpenGL.cmake
@@ -2,7 +2,7 @@
 
 find_library(NVIDIA_OPENGL_gl_LIBRARY
   GL
-  PATHS /usr/lib/nvidia-331 /usr/lib/nvidia-340
+  PATHS /usr/lib/nvidia-331 /usr/lib/nvidia-340 /usr/lib/nvidia-346 
   NO_DEFAULT_PATH
   NO_SYSTEM_ENVIRONMENT_PATH
 )
