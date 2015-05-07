6b4a0c903742ad15499253e5de392be258c2d2ba
Juan Jose Garcia <juanjosegarciacan@gmail.com>
2015-05-07 16:37:04 +0200

    Cmake common config added

---
+++ CMake/CPackConfig.cmake
@@ -0,0 +1,7 @@
+set(CPACK_PACKAGE_CONTACT "Juan Jose Garcia <juanjose.garcia@urjc.es>")
+set(CPACK_PACKAGE_DESCRIPTION_FILE "${PROJECT_SOURCE_DIR}/README.md")
+set(CPACK_PACKAGE_LICENSE "Proprietary")
+
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "")
+
+include(CommonCPack)
