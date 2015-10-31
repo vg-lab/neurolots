c0d9950314a0e8020996b363339a07e76113740b
Pablo Toharia <pablo.toharia@urjc.es>
2015-10-31 13:17:03 +0100

    Fixed for CI building.

---
+++ .jambs_configs.sh
@@ -0,0 +1,6 @@
+git clone git@gitlab.gmrv.es:common/jambs-config-nsviz.git
+git clone git@gitlab.gmrv.es:common/jambs-config-hbp.git
+export JAMBS_nsol_repo_tag=de7f1c7
+export JAMBS_zeq_repo_tag=013ba7a
+export JAMBS_gmrvzeq_repo_tag=8e4132d
+export JAMBS_deflect_repo_tag=160ff53
