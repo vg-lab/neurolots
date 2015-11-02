a70a122da944979fbf1b61b1598882ea55b099f3
Pablo Toharia <pablo.toharia@urjc.es>
2015-11-02 15:45:06 +0100

    Bump to dependencies tags.

--- .jambs_configs.sh
+++ .jambs_configs.sh
@@ -2,5 +2,5 @@ git clone git@gitlab.gmrv.es:common/jambs-config-nsviz.git
 git clone git@gitlab.gmrv.es:common/jambs-config-hbp.git
 export JAMBS_nsol_repo_tag=de7f1c7
 export JAMBS_zeq_repo_tag=013ba7a
-export JAMBS_gmrvzeq_repo_tag=5ce9f58
+export JAMBS_gmrvzeq_repo_tag=883abbb
 export JAMBS_deflect_repo_tag=160ff53
