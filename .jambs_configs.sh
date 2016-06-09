b9e83bb06fd80d191e31bae0f806be4911a5b79d
Juan Jose Garcia <juanjosegarciacan@gmail.com>
2016-06-09 10:16:45 +0200

    Added brion tag to OSX-RequiredDeps build

--- .jambs_configs.sh
+++ .jambs_configs.sh
@@ -1,6 +1,7 @@
 git clone git@gitlab.gmrv.es:common/jambs-config-nsviz.git
 git clone git@gitlab.gmrv.es:common/jambs-config-hbp.git
 export JAMBS_nsol_repo_tag=72630e88
+export JAMBS_brion_repo_tag=aead60cc
 export JAMBS_zeq_repo_tag=e6bf42e
 export JAMBS_gmrvzeq_repo_tag=dd922c0
 export JAMBS_deflect_repo_tag=160ff53
